b23ee7519de45df9f6fed28c8faf71c5970fe894
Matěj Brachtl <bra42176@spseol.cz>
2025-07-09 16:47:51 +0200

    Selection Design 0.1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ItemComponent } from './item/item.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ItemComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angumyk';
+  items = [
+    { id: 1, name: 'Ovčí mléko pro děti Omlík', price: 55.55 },
+    { id: 2, name: 'Item 2', price: 20 },
+    { id: 3, name: 'Item 3', price: 30 }
+  ];
 }
---
+++ src/app/item/item.component.ts
@@ -0,0 +1,34 @@
+import { Component, Input, ElementRef, ViewChild } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // Import FormsModule for ngModel
+
+@Component({
+  selector: 'cart-item',
+  imports: [FormsModule],
+  templateUrl: './item.component.html',
+  styleUrl: './item.component.css'
+})
+export class ItemComponent {
+  @Input() item: { id: number; name: string; price: number };
+
+  constructor() {
+    this.item = { id: 0, name: '#nic#', price: 0 };
+  }
+
+  quantity: number = 1;
+
+  get totalPrice(): number {
+    return this.item ? this.item.price*this.quantity : 0;
+  }
+
+  @ViewChild('itemDiv', { static: true }) itemDiv!: ElementRef;
+  @ViewChild('addCartButton', { static: true }) addCartButton!: ElementRef;
+
+  addToCart() {
+    // change div style background color to green
+    
+    this.itemDiv.nativeElement.setAttribute('style', 'background-color: #f0f0f0;'); 
+    this.addCartButton.nativeElement.setAttribute('style', 'background-color: #f0f0f0;');
+    
+    console.log(`Přidáno do košíku: ${this.item.name}, množství: ${this.quantity}, cena: ${this.totalPrice} Kč`);
+  }
+}
